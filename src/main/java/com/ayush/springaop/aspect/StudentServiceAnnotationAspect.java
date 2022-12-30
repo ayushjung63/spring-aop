e491ce9821764fd73b3791810f017997bba472a8
Ayush <ayushbasnet2020@gmail.com>
2022-12-30 09:13:26 +0545

    fix: generic param log

--- src/main/java/com/ayush/springaop/aspect/StudentServiceAnnotationAspect.java
+++ src/main/java/com/ayush/springaop/aspect/StudentServiceAnnotationAspect.java
@@ -1,6 +1,9 @@
 package com.ayush.springaop.aspect;
 
+import com.ayush.springaop.model.Student;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -25,8 +28,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class StudentServiceAnnotationAspect {
 
-    @Before("@annotation(com.ayush.springaop.annotation.LogRequestData) && args(studentId)")
-    public void logGetRequest(Long studentId){
-        log.info("--------id---------- "+studentId+" ------------------");
+
+    @Before("@annotation(com.ayush.springaop.annotation.LogRequestData) && args(data)")
+    public void logGetRequest(JoinPoint joinPoints, Object data){     /* ADVICE */
+        if (data instanceof Long){
+            Long studentId = Long.parseLong(String.valueOf(data));
+            Signature signature = joinPoints.getSignature();
+            String signatureName = signature.getName();
+            log.info("-----"+signatureName+"------------------id-------"+studentId+"---------------");
+        }else {
+            Student student = (Student) data;
+            log.info("--------"+student.getId()+"---------"+student.getFullName()+"---------------");
+        }
     }
 }
--- src/main/java/com/ayush/springaop/controller/StudentController.java
+++ src/main/java/com/ayush/springaop/controller/StudentController.java
@@ -6,10 +6,7 @@ import com.ayush.springaop.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,9 +23,15 @@ public class StudentController {
         return new ResponseEntity(studentList, HttpStatus.OK);
     }
 
-    @LogRequestData
+    @LogRequestData /* JOIN POINT */
     @GetMapping("/get/{studentId}")
     public ResponseEntity fetchStudentList(@PathVariable("studentId") Long studentId){
        return new ResponseEntity(studentId,HttpStatus.OK);
+    }
+
+    @LogRequestData /* JOIN POINT */
+    @PostMapping("/save")
+    public ResponseEntity saveStudent(@RequestBody Student student){
+       return new ResponseEntity(student,HttpStatus.OK);
     }
 }
